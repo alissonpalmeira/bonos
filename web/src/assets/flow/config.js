6936d339b47a6d982cd0d7bd9e5dbb015b92d064
Alisson Palmeira <alisson.palmeira@gmail.com>
2023-02-27 17:52:45 -0300

    update environment variables Co-authored-by: 2iegocarvalho <2iegocarvalho@users.noreply.github.com>

--- web/src/assets/flow/config.js
+++ web/src/assets/flow/config.js
@@ -3,7 +3,7 @@ import { config } from "@onflow/fcl";
 config({
     "app.detail.title": "Bonos",
     "app.detail.icon": process.env.REACT_APP_LOGO_URL,
-    "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://rest-mainnet.onflow.org"
-    "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
-    "0xBonos": "0x9ae006f4fcb04a9a",
+    "accessNode.api": process.env.REACT_APP_ACCESS_NODE_API,
+    "discovery.wallet": process.env.REACT_APP_DISCOVERY_WALLET,
+    "0xBonos": process.env.REACT_APP_BONOS_ACCOUNT,
 })
